ce8bb670f50a8be2629b26d419f3187d87165684
KANTNOLI <zahargysev02@gmail.com>
2023-05-03 23:10:00 +0300

    Add project files.

---
+++ StudentCppRevisionTask08Project/task01.cpp
@@ -0,0 +1,49 @@
+﻿#include "tasks.h"
+
+/*	Task 01. The same Numbers [совпадающие числа]
+ *
+ *	Даны четыре целых числа. Определите, сколько из них совпадающих.
+ *	Если числа не совпадают, то необходимо возвратить ноль.  
+ * 
+ *	Формат входных данных [input]
+ *	На вход даётся четыре целых числа в диапазоне типа int.
+ *
+ *	Формат выходных данных [output]
+ *	Возвратите количество совпадающих чисел или ноль, если таких чисел нет.
+ *
+ *	[ input 1]: 7 7 7 7
+ *	[output 1]: 4
+ *
+ *	[ input 2]: 7 7 7 8
+ *	[output 2]: 3
+ *
+ *	[ input 3]: 7 7 8 9
+ *	[output 3]: 2
+ *
+ *	[ input 4]: 6 7 8 9
+ *	[output 4]: 0
+ */
+
+int task01(int a, int b, int c, int d) {
+	int count = 0;
+	int size = 4;
+	int* vector = new int[a, b, c, d];
+
+	for (int j = 0; j < size; j++)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			int first = vector[j];
+			int second = vector[i];
+
+			if (first == second && j != i) {
+				count++;
+			}
+		}
+	}
+
+	return count;
+}
+
+
+
